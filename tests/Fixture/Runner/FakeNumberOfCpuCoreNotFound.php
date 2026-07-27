5691f4e83264141a0631f0a0426200526dcc603d
Ben Challis <ben-challis@users.noreply.github.com>
2026-07-28 00:46:51 +0100

    test: cover optional CPU counter (#336)

---
+++ tests/Fixture/Runner/FakeNumberOfCpuCoreNotFound.php
@@ -0,0 +1,9 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Greenlight\Tests\Fixture\Runner;
+
+use Greenlight\Doubles\Fake;
+
+final class FakeNumberOfCpuCoreNotFound extends \RuntimeException implements Fake {}
