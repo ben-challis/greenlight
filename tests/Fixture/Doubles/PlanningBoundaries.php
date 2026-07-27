b7949b1ce2c9d68e416377021dc4b4b1164a4716
Ben Challis <ben@lendable.co.uk>
2026-07-27 18:13:00 +0100

    test: cover mock planning boundaries

---
+++ tests/Fixture/Doubles/PlanningBoundaries.php
@@ -0,0 +1,14 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Greenlight\Tests\Fixture\Doubles;
+
+class PlanningBoundaries
+{
+    public static function staticMethod(): void {}
+
+    protected function protectedMethod(): void {}
+
+    final public function finalMethod(): void {}
+}
