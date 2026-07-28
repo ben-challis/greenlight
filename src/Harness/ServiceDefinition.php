5c45831973ea567faef2eaf82c69e550e82931dd
Ben Challis <ben@lendable.co.uk>
2026-07-28 10:15:12 +0100

    fix(harness): reject empty service definitions

--- src/Harness/ServiceDefinition.php
+++ src/Harness/ServiceDefinition.php
@@ -11,14 +11,27 @@ namespace Greenlight\Harness;
 final readonly class ServiceDefinition
 {
     /**
+     * @var class-string
+     */
+    public string $type;
+
+    /**
      * @template T of object
      *
-     * @param class-string<T> $type
+     * @param class-string<T>|'' $type
      * @param \Closure(): T $factory
+     *
+     * @throws \InvalidArgumentException
      */
     public function __construct(
-        public string $type,
+        string $type,
         public Scope $scope,
         public \Closure $factory,
-    ) {}
+    ) {
+        if ($type === '') {
+            throw new \InvalidArgumentException('Harness service type MUST NOT be empty.');
+        }
+
+        $this->type = $type;
+    }
 }
