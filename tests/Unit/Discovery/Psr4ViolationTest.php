d18d52f9ebd4e27c7dfe31cf849b5519f8cfd5be
Ben Challis <ben@lendable.co.uk>
2026-07-28 01:09:42 +0100

    test: remove redundant directory guard

--- tests/Unit/Discovery/Psr4ViolationTest.php
+++ tests/Unit/Discovery/Psr4ViolationTest.php
@@ -75,10 +75,6 @@ final readonly class Psr4ViolationTest
     {
         $directory = $this->tempDirectory->subdirectory('incomplete-declaration');
 
-        if ($directory === '') {
-            Fail::because('Expected the incomplete class fixture directory to be non-empty.');
-        }
-
         $file = $directory . '/IncompleteTest.php';
         \file_put_contents($file, "<?php\n\nclass");
         $resolvedFile = \realpath($file);
