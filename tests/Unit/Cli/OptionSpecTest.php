e4abf735d4c6191ab312d79437d4dc838af5ce47
Ben Challis <ben@lendable.co.uk>
2026-07-29 04:33:29 +0100

    test(cli): retain zero option names

--- tests/Unit/Cli/OptionSpecTest.php
+++ tests/Unit/Cli/OptionSpecTest.php
@@ -6,11 +6,22 @@ namespace Greenlight\Tests\Unit\Cli;
 
 use Greenlight\Attribute\DataSet;
 use Greenlight\Attribute\Test;
+use Greenlight\Cli\ArgumentParser;
 use Greenlight\Cli\OptionSpec;
 use Greenlight\Expect\Expect;
 
 final readonly class OptionSpecTest
 {
+    #[Test]
+    public function zeroStringLongNameParsesAsAFlag(): void
+    {
+        $arguments = new ArgumentParser([new OptionSpec('0')])->parse(['--0']);
+
+        Expect::that($arguments->has('0'))
+            ->because('a zero-string long option name is not empty')
+            ->toBeTrue();
+    }
+
     #[Test]
     #[DataSet('invalidDefinitions')]
     public function invalidOptionDefinitionsAreRejected(
