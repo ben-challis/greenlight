c2129c79773749d4ff7a084a3d391dd51391c1ee
Ben Challis <ben@lendable.co.uk>
2026-08-16 02:38:21 +0100

    test: give attribute contracts independent IDs

--- tests/Unit/Attribute/AttributesTest.php
+++ tests/Unit/Attribute/AttributesTest.php
@@ -6,6 +6,7 @@ namespace Greenlight\Tests\Unit\Attribute;
 
 use Greenlight\Attribute\After;
 use Greenlight\Attribute\Before;
+use Greenlight\Attribute\DataSet;
 use Greenlight\Attribute\Test;
 use Greenlight\Expect\Expect;
 
@@ -25,17 +26,29 @@ final class AttributesTest
         Expect::that($this->beforeRan)->because('before hook runs before tests')->toBeTrue();
     }
 
+    /**
+     * @param class-string<Test|Before|After> $attributeClass
+     */
     #[Test]
-    public function attributesTargetMethods(): void
+    #[DataSet('methodAttributes')]
+    public function attributesTargetMethods(string $attributeClass): void
     {
-        foreach ([Test::class, Before::class, After::class] as $attributeClass) {
-            $attributes = new \ReflectionClass($attributeClass)->getAttributes(\Attribute::class);
+        $attributes = new \ReflectionClass($attributeClass)->getAttributes(\Attribute::class);
 
-            Expect::that($attributes)->toHaveCount(1);
+        Expect::that($attributes)->toHaveCount(1);
 
-            $flags = $attributes[0]->newInstance()->flags;
+        $flags = $attributes[0]->newInstance()->flags;
 
-            Expect::that($flags)->toBe(\Attribute::TARGET_METHOD);
-        }
+        Expect::that($flags)->toBe(\Attribute::TARGET_METHOD);
+    }
+
+    /**
+     * @return iterable<string, array{class-string<Test|Before|After>}>
+     */
+    public static function methodAttributes(): iterable
+    {
+        yield 'Test' => [Test::class];
+        yield 'Before' => [Before::class];
+        yield 'After' => [After::class];
     }
 }
