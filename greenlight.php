6982aa82a06a1785b66767076905d61896b83209
Ben Challis <ben@lendable.co.uk>
2026-08-22 16:46:33 +0100

    docs: remove fixture path comment

--- greenlight.php
+++ greenlight.php
@@ -6,9 +6,6 @@ use Greenlight\Config\GreenlightConfig;
 use Greenlight\Config\StorageBuilder;
 
 return GreenlightConfig::create()
-    // Do not add tests/Fixture to these paths.
-    // Some fixture tests intentionally call sleep(60) or exit(9), or retain memory.
-    // Acceptance tests run these fixture suites in separate processes.
     ->paths(['tests/Unit', 'tests/Acceptance'])
     ->workers(count: 'auto')
     ->resourceLimit('analysis-process', 5)
