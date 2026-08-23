01cd6897e07692e38cf09673d791fbf43945f210
Ben Challis <ben@lendable.co.uk>
2026-08-23 13:52:36 +0100

    refactor(symfony): invoke kernel factory directly

--- src/Symfony/SymfonyPlugin.php
+++ src/Symfony/SymfonyPlugin.php
@@ -140,7 +140,7 @@ final class SymfonyPlugin implements AfterTestSubscriber, HarnessProvider, Servi
             return $this->kernel;
         }
 
-        $kernel = $this->createKernel();
+        $kernel = ($this->factory)();
 
         if (!$kernel instanceof KernelInterface) {
             throw SymfonyBridgeError::notAKernelFromFactory(\get_debug_type($kernel));
@@ -171,11 +171,6 @@ final class SymfonyPlugin implements AfterTestSubscriber, HarnessProvider, Servi
         $this->kernel = $kernel;
 
         return $kernel;
-    }
-
-    private function createKernel(): mixed
-    {
-        return ($this->factory)();
     }
 
     /**
