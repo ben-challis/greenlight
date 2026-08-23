d7dff089f27b8b74e0d9b59c606a6eac69434562
Ben Challis <ben-challis@users.noreply.github.com>
2026-08-23 09:22:28 +0100

    refactor(test): throw aggregate cleanup failures (#1589)

---
+++ src/Test/CleanupFailed.php
@@ -0,0 +1,30 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Greenlight\Test;
+
+/**
+ * One or more cleanup callbacks failed.
+ *
+ * @internal
+ */
+final class CleanupFailed extends \RuntimeException
+{
+    /**
+     * @param non-empty-list<\Throwable> $failures
+     */
+    private function __construct(
+        public readonly array $failures,
+    ) {
+        parent::__construct('Test cleanup failed.');
+    }
+
+    /**
+     * @param non-empty-list<\Throwable> $failures
+     */
+    public static function fromFailures(array $failures): self
+    {
+        return new self($failures);
+    }
+}
